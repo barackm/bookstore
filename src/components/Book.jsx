2543ad364bff66e69fa941c0f9c6a7f95563d08f
Barack Mukelenga <barackmukelenga100@gmail.com>
2021-08-02 12:19:31 +0200

    Fix eslint errors

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,3 +1,4 @@
-const BookDetail = () => (
-<h1> Book Details </h1>
-)
+import React from 'react';
+
+const BookDetail = () => <h1> Book Details </h1>;
+export default BookDetail;
--- src/containers/BooksList.jsx
+++ src/containers/BooksList.jsx
@@ -7,25 +7,26 @@ class BooksList extends Component {
   }
 
   render() {
-    return ( 
-  <table style="width:100%">
-  <tr>
-  <th>Firstname</th>
-  <th>Lastname</th>
-  <th>Age</th>
-  </tr>
-  <tr>
-  <td>Jill</td>
-  <td>Smith</td>
-  <td>50</td>
-  </tr>
-  <tr>
-  <td>Eve</td>
-  <td>Jackson</td>
-  <td>94</td>
-  </tr>
-  </table> 
-    )}
+    return (
+      <table>
+        <tr>
+          <th>Firstname</th>
+          <th>Lastname</th>
+          <th>Age</th>
+        </tr>
+        <tr>
+          <td>Jill</td>
+          <td>Smith</td>
+          <td>50</td>
+        </tr>
+        <tr>
+          <td>Eve</td>
+          <td>Jackson</td>
+          <td>94</td>
+        </tr>
+      </table>
+    );
+  }
 }
 
 export default BooksList;
